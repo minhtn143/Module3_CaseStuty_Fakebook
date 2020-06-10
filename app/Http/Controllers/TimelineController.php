8c018eed0ccb81cccaf6f5b8d3411df09399547b
minhtn143 <minhtn143@gmail.com>
2020-06-10 16:00:02 +0700

    Done post and show in timeline

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use SplStack;
 
 class TimelineController extends Controller
 {
     public function index()
     {
-        $posts = Post::get();
+        $datas = Post::all();
+        $posts = new SplStack();
+        foreach ($datas as $value) {
+            $posts->push($value);
+        }
+
         return view('timeline.index', compact('posts'));
     }
 }

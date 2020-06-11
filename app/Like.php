63e0873551f4772966534da717ada90f228c523f
minhtn143 <minhtn143@gmail.com>
2020-06-11 13:38:28 +0700

    merge from dev

--- app/Like.php
+++ app/Like.php
@@ -7,16 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     //
-    protected $fillable = ['user_id', 'post_id'];
     protected $table = "likes";
     protected $fillable = [
         'user_id',
+        'post_id'
     ];
-
-    public function likeable()
-    {
-        return $this->morphTo();
-    }
 
     public function likeable()
     {

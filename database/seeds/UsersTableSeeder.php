f4da00082c6d7f0d958fa13ae9b1730ffbd3d0e5
Nguyenhieuanh <nguyenhieuanh.061626@gmail.com>
2020-06-13 20:11:36 +0700

    fake database user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,26 +17,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
-        $user1 = App\User::create([
-            'first_name' => Str::random(10),
-            'last_name' => Str::random(5),
-            'email' => 'minh@gmail.com',
-            'password' => Hash::make('12345678')
-        ]);
-
-        $user2 = App\User::create([
-            'first_name' => Str::random(10),
-            'last_name' => Str::random(5),
-            'email' => 'hieu@gmail.com',
-            'password' => Hash::make('12345678')
-        ]);
-
-        $user3 = App\User::create([
-            'first_name' => Str::random(10),
-            'last_name' => Str::random(5),
-            'email' => 'tung@gmail.com',
-            'password' => Hash::make('12345678')
-        ]);
+        factory(User::class, 30)->create();
     }
 }
